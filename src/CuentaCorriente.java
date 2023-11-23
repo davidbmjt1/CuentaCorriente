d3c888bce38ba46b513936fd4f893a8e76bbdd2b
David <davidbmjt@hotmail.com>
2023-11-23 13:26:00 +0100

    App cuenta corriente

---
+++ src/CuentaCorriente.java
@@ -0,0 +1,33 @@
+public class CuentaCorriente {
+    String nombreTitular;
+    double saldo;
+    String numCuenta;
+
+    public CuentaCorriente (String nombreTitular, double saldo, String numCuenta){
+        this.nombreTitular = nombreTitular;
+        this.saldo = saldo;
+        this.numCuenta = numCuenta;
+    }
+
+
+    public void retirarFondos(double cantidad) {
+        saldo = saldo - cantidad;
+    }
+
+    public void inregresarFondo(double cantidad) {
+        saldo += cantidad;
+    }
+
+
+    /*
+    public void mostrarSaldo() {
+    }
+    */
+
+
+    @Override
+    public String toString(){
+        return STR."\{numCuenta} | \{nombreTitular} | \{saldo}";
+    }
+
+}
